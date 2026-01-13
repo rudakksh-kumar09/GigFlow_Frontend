626a8ad56b91a198d69ee93c6340984ec4e2a67b
Rudraksh-Kumar09 <rudrakshkumar519@gmail.com>
2026-01-13 10:37:45 +0530

    Vercal_change

--- Server/server.js
+++ Server/server.js
@@ -31,6 +31,10 @@ app.use(cookieParser());
 
 app.set('io', io);
 
+app.get('/', (req, res) => {
+  res.json({ message: 'GigFlow API is running' });
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/gigs', gigRoutes);
 app.use('/api/bids', bidRoutes);
@@ -51,3 +55,5 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+export default app;
